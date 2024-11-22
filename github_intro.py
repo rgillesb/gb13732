9b66e2e0b1d1c0bccfb957cf13fadf29637dd75a
rgillesb <gilles.bokolo@math.byu.edu>
2024-11-22 12:25:42 -0700

    python intro lab

---
+++ github_intro.py
@@ -0,0 +1,20 @@
+# github_intro.py
+"""Volume 1: Introduction to GitHub.
+<Gilles Bokolo>
+<MATH 695R-HWK1>
+<September 13,2024>
+"""
+# Problem 1
+def problem1():
+    """ Return your first and last name as a string."""
+    # Replace "First" and "Last" with your actual first and last name
+    return "Gilles Bokolo"
+
+def problem2():
+    """practice making a merge conflict with yourself by returning
+    different strings in different places.
+    """
+    # To create a merge conflict, change the return value of this function
+    # in two separate branches or commits before attempting to merge.
+    # Combine both versions
+    return "This is the resolved version of the function."
